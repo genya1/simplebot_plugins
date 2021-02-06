970119fda4a07bac354e1ed29905b086ca07dc47
adbenitez <asieldbenitez@gmail.com>
2021-02-05 19:23:36 -0500

    score: set isolation_level in DB manager

--- plugins/simplebot_score/simplebot_score/db.py
+++ plugins/simplebot_score/simplebot_score/db.py
@@ -4,7 +4,7 @@ import sqlite3
 
 class DBManager:
     def __init__(self, db_path: str) -> None:
-        self.db = sqlite3.connect(db_path, check_same_thread=False)
+        self.db = sqlite3.connect(db_path, check_same_thread=False, isolation_level=None)
         self.db.row_factory = sqlite3.Row
         with self.db:
             self.db.execute(
