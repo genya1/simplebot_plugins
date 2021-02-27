1a78572603b4dcb561c3158620037c314160465a
adbenitez <asieldbenitez@gmail.com>
2021-02-27 03:17:01 -0500

    avatar: fix linter warnings

--- plugins/simplebot_avatar/simplebot_avatar/__init__.py
+++ plugins/simplebot_avatar/simplebot_avatar/__init__.py
@@ -9,9 +9,10 @@ from simplebot.bot import Replies
 from simplebot.commands import IncomingCommand
 from simplebot.hookspec import deltabot_hookimpl
 
-version = '1.0.0'
+__version__ = '1.0.0'
 HEADERS = {
-    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0)'
+    ' Gecko/20100101 Firefox/60.0'}
 
 
 # ======== Hooks ===============
@@ -25,13 +26,15 @@ def deltabot_init(bot: DeltaBot) -> None:
 # ======== Commands ===============
 
 def cmd_cat(command: IncomingCommand, replies: Replies) -> None:
-    """Generate a cat avatar based on the given text, if no text is given a random avatar is generated.
+    """Generate a cat avatar based on the given text.
+    If no text is given a random avatar is generated.
     """
     replies.add(**get_message(command.payload, '2016_cat-generator'))
 
 
 def cmd_bird(command: IncomingCommand, replies: Replies) -> None:
-    """Generate a bird avatar based on the given text, if no text is given a random avatar is generated.
+    """Generate a bird avatar based on the given text.
+    If no text is given a random avatar is generated.
     """
     replies.add(**get_message(command.payload, '2019_bird-generator'))
 
@@ -41,14 +44,14 @@ def cmd_bird(command: IncomingCommand, replies: Replies) -> None:
 def get_message(text: str, generator: str) -> dict:
     url = 'https://www.peppercarrot.com/extras/html/{}/'.format(generator)
     if not text:
-        with requests.get(url, headers=HEADERS) as r:
-            r.raise_for_status()
-            soup = bs4.BeautifulSoup(r.text, 'html.parser')
+        with requests.get(url, headers=HEADERS) as resp:
+            resp.raise_for_status()
+            soup = bs4.BeautifulSoup(resp.text, 'html.parser')
         text = soup.find('img', class_='avatar')[
             'src'].rsplit('=', maxsplit=1)[-1]
 
     url += 'avatar.php?seed=' + quote_plus(text)
-    with requests.get(url, headers=HEADERS) as r:
-        r.raise_for_status()
+    with requests.get(url, headers=HEADERS) as resp:
+        resp.raise_for_status()
         return dict(text=text, filename='catvatar.png',
-                    bytefile=io.BytesIO(r.content))
+                    bytefile=io.BytesIO(resp.content))
