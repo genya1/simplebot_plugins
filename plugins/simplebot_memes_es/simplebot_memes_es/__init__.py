0a44583e2b248a36b407a991e30070c8177325b0
adbenitez <asieldbenitez@gmail.com>
2021-03-12 05:35:18 -0500

    memes_es: use new simplebot API

--- plugins/simplebot_memes_es/simplebot_memes_es/__init__.py
+++ plugins/simplebot_memes_es/simplebot_memes_es/__init__.py
@@ -5,10 +5,9 @@ import re
 
 import bs4
 import requests
+import simplebot
 from simplebot import DeltaBot
 from simplebot.bot import Replies
-from simplebot.commands import IncomingCommand
-from simplebot.hookspec import deltabot_hookimpl
 
 __version__ = '1.0.0'
 ua = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101'
@@ -16,65 +15,55 @@ ua += ' Firefox/60.0'
 HEADERS = {
     'user-agent': ua
 }
-dbot: DeltaBot
 
 
-# ======== Hooks ===============
-
-@deltabot_hookimpl
+@simplebot.hookimpl
 def deltabot_init(bot: DeltaBot) -> None:
-    global dbot
-    dbot = bot
-    getdefault('max_meme_size', 1024*200)
-
-    bot.commands.register(name='/cuantarazon', func=cmd_cuantarazon)
-    bot.commands.register(name='/cuantocabron', func=cmd_cuantocabron)
+    _getdefault(bot, 'max_meme_size', 1024*200)
 
 
-# ======== Commands ===============
-
-
-def cmd_cuantarazon(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def cuantarazon(bot: DeltaBot, replies: Replies) -> None:
     """Devuelve un meme al azar de https://m.cuantarazon.com
     """
-    replies.add(**get_meme('https://m.cuantarazon.com/aleatorio/'))
+    replies.add(**_get_meme(bot, 'https://m.cuantarazon.com/aleatorio/'))
 
 
-def cmd_cuantocabron(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def cuantocabron(bot: DeltaBot, replies: Replies) -> None:
     """Devuelve un meme al azar de https://m.cuantocabron.com
     """
-    replies.add(**get_meme('https://m.cuantocabron.com/aleatorio'))
+    replies.add(**_get_meme(bot, 'https://m.cuantocabron.com/aleatorio'))
 
 
-# ======== Utilities ===============
+def _get_image(url: str) -> tuple:
+    with requests.get(url, headers=HEADERS) as res:
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    img = soup('div', class_='storyContent')[-1].img
+    return (img['title'], img['src'])
 
-def get_meme(url: str) -> dict:
-    def get_image(url: str) -> tuple:
-        with requests.get(url, headers=HEADERS) as r:
-            r.raise_for_status()
-            soup = bs4.BeautifulSoup(r.text, 'html.parser')
-        img = soup('div', class_='storyContent')[-1].img
-        return (img['title'], img['src'])
 
+def _get_meme(bot: DeltaBot, url: str) -> dict:
     img = b''
-    max_meme_size = int(getdefault('max_meme_size'))
+    max_meme_size = int(_getdefault(bot, 'max_meme_size'))
     for _ in range(10):
-        img_desc, img_url = get_image(url)
+        img_desc, img_url = _get_image(url)
         with requests.get(img_url, headers=HEADERS) as r:
             r.raise_for_status()
             if len(r.content) <= max_meme_size:
                 img = r.content
-                ext = get_ext(r) or '.jpg'
+                ext = _get_ext(r) or '.jpg'
                 break
             if not img or len(img) > len(r.content):
                 img = r.content
-                ext = get_ext(r) or '.jpg'
+                ext = _get_ext(r) or '.jpg'
 
     text = '{}\n\n{}'.format(img_desc, img_url)
     return dict(text=text, filename='meme'+ext, bytefile=io.BytesIO(img))
 
 
-def get_ext(r) -> str:
+def _get_ext(r) -> str:
     d = r.headers.get('content-disposition')
     if d is not None and re.findall("filename=(.+)", d):
         fname = re.findall(
@@ -95,9 +84,9 @@ def get_ext(r) -> str:
     return ext
 
 
-def getdefault(key: str, value=None) -> str:
-    val = dbot.get(key, scope=__name__)
+def _getdefault(bot: DeltaBot, key: str, value=None) -> str:
+    val = bot.get(key, scope=__name__)
     if val is None and value is not None:
-        dbot.set(key, value, scope=__name__)
+        bot.set(key, value, scope=__name__)
         val = value
     return val
