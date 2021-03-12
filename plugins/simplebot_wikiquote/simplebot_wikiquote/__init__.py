00808b66b5b13d5289d0486e30fe654f86cebda1
adbenitez <asieldbenitez@gmail.com>
2021-03-12 05:14:51 -0500

    wikiquote: use new simplebot API

--- plugins/simplebot_wikiquote/simplebot_wikiquote/__init__.py
+++ plugins/simplebot_wikiquote/simplebot_wikiquote/__init__.py
@@ -1,54 +1,44 @@
 
 from random import choice
 
+import simplebot
 import wikiquote as wq
+from deltachat import Message
 from simplebot import DeltaBot
 from simplebot.bot import Replies
-from simplebot.commands import IncomingCommand
-from simplebot.hookspec import deltabot_hookimpl
 
 __version__ = '1.0.0'
 
 
-# ======== Hooks ===============
-
-@deltabot_hookimpl
-def deltabot_init(bot: DeltaBot) -> None:
-    bot.commands.register(name="/quote", func=cmd_quote)
-
-
-# ======== Commands ===============
-
-def cmd_quote(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command(name='/quote')
+def cmd_quote(bot: DeltaBot, payload: str, message: Message,
+              replies: Replies) -> None:
     """Get Wikiquote quotes.
 
     Search in Wikiquote or get the quote of the day if no text is given.
     Example: `/quote Richard Stallman`
     """
-    locale = get_locale(
-        command.bot, command.message.get_sender_contact().addr)
+    locale = _get_locale(bot, message.get_sender_contact().addr)
     if locale in wq.supported_languages():
         lang = locale
     else:
         lang = None
-    if command.payload:
-        authors = wq.search(command.payload, lang=lang)
+    if payload:
+        authors = wq.search(payload, lang=lang)
         if authors:
-            if command.payload.lower() == authors[0].lower():
+            if payload.lower() == authors[0].lower():
                 author = authors[0]
             else:
                 author = choice(authors)
             quote = '"{}"\n\n― {}'.format(
                 choice(wq.quotes(author, max_quotes=200, lang=lang)), author)
         else:
-            quote = 'No quote found for: {}'.format(command.payload)
+            quote = 'No quote found for: {}'.format(payload)
     else:
         quote = '"{}"\n\n― {}'.format(*wq.quote_of_the_day(lang=lang))
 
     replies.add(text=quote)
 
 
-# ======== Utilities ===============
-
-def get_locale(bot, addr: str) -> str:
+def _get_locale(bot, addr: str) -> str:
     return bot.get('locale', scope=addr) or bot.get('locale') or 'en'
