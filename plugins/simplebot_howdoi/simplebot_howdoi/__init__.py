bc07af3d984fa5f6c208a3bad4052c18a50706b7
adbenitez <asieldbenitez@gmail.com>
2021-03-12 04:53:33 -0500

    howdoi: use new simplebot API

--- plugins/simplebot_howdoi/simplebot_howdoi/__init__.py
+++ plugins/simplebot_howdoi/simplebot_howdoi/__init__.py
@@ -1,31 +1,17 @@
 import json
-from threading import Thread
 
+import simplebot
 from howdoi.howdoi import howdoi
-from simplebot import DeltaBot
 from simplebot.bot import Replies
-from simplebot.commands import IncomingCommand
-from simplebot.hookspec import deltabot_hookimpl
 
 __version__ = '1.0.0'
 
 
-@deltabot_hookimpl
-def deltabot_init(bot: DeltaBot) -> None:
-    bot.commands.register(name="/howdoi", func=cmd_howdoi)
-
-
-def cmd_howdoi(command: IncomingCommand, replies: Replies) -> None:
-    """Instant coding answers. Example: /howdoi format date bash
-    """
-    Thread(target=_howdoi, args=(command,), daemon=True).start()
-
-
-def _howdoi(command: IncomingCommand) -> None:
+@simplebot.command(name='/howdoi')
+def cmd_howdoi(payload: str, replies: Replies) -> None:
+    """Instant coding answers. Example: /howdoi format date bash"""
     try:
-        res = json.loads(howdoi("{} -j".format(command.payload)))[0]
-        text = '{}\n\n↗️ {}'.format(res['answer'], res['link'])
-    except Exception as ex:
-        command.bot.logger.exception(ex)
-        text = 'Something went wrong.'
-    command.message.chat.send_text(text)
+        res = json.loads(howdoi('{} -j'.format(payload)))[0]
+        replies.add(text='{}\n\n↗️ {}'.format(res['answer'], res['link']))
+    except (Exception, SystemExit):
+        replies.add(text='Something went wrong.')
