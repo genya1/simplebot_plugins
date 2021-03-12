5efbed36297c0ca3a3563a22c0889f82aadc163e
adbenitez <asieldbenitez@gmail.com>
2021-03-12 05:31:25 -0500

    irc: use new simplebot API

--- plugins/simplebot_irc/simplebot_irc/__init__.py
+++ plugins/simplebot_irc/simplebot_irc/__init__.py
@@ -4,68 +4,50 @@ import re
 from threading import Thread
 from time import sleep
 
+import simplebot
 from deltachat import Chat, Contact, Message
 from simplebot import DeltaBot
 from simplebot.bot import Replies
-from simplebot.commands import IncomingCommand
-from simplebot.hookspec import deltabot_hookimpl
 
 from .database import DBManager
 from .irc import IRCBot
 
 __version__ = '1.0.0'
 nick_re = re.compile(r'[-_a-zA-Z0-9]{1,30}$')
-dbot: DeltaBot
 db: DBManager
 irc_bridge: IRCBot
 
 
-# ======== Hooks ===============
-
-@deltabot_hookimpl
+@simplebot.hookimpl
 def deltabot_init(bot: DeltaBot) -> None:
-    global dbot
-    dbot = bot
-
-    getdefault('nick', 'SimpleBot')
-    getdefault('host', 'irc.freenode.net')
-    getdefault('port', '6667')
-
-    bot.filters.register(name=__name__, func=filter_messages)
-
-    dbot.commands.register('/join', cmd_join)
-    dbot.commands.register('/remove', cmd_remove)
-    dbot.commands.register('/topic', cmd_topic)
-    dbot.commands.register('/names', cmd_members)
-    dbot.commands.register('/nick', cmd_nick)
-    dbot.commands.register('/me', cmd_me)
+    _getdefault(bot, 'nick', 'SimpleBot')
+    _getdefault(bot, 'host', 'irc.freenode.net')
+    _getdefault(bot, 'port', '6667')
 
 
-@deltabot_hookimpl
+@simplebot.hookimpl
 def deltabot_start(bot: DeltaBot) -> None:
     global db, irc_bridge
-
-    db = get_db(bot)
-
-    nick = getdefault('nick')
-    host = getdefault('host')
-    port = int(getdefault('port'))
+    db = _get_db(bot)
+    nick = _getdefault(bot, 'nick')
+    host = _getdefault(bot, 'host')
+    port = int(_getdefault(bot, 'port'))
     irc_bridge = IRCBot(host, port, nick, db, bot)
-    Thread(target=run_irc, daemon=True).start()
+    Thread(target=_run_irc, args=(bot,), daemon=True).start()
 
 
-@deltabot_hookimpl
+@simplebot.hookimpl
 def deltabot_member_added(chat: Chat, contact: Contact) -> None:
     channel = db.get_channel_by_gid(chat.id)
     if channel:
         irc_bridge.preactor.join_channel(contact.addr, channel)
 
 
-@deltabot_hookimpl
-def deltabot_member_removed(chat: Chat, contact: Contact) -> None:
+@simplebot.hookimpl
+def deltabot_member_removed(bot: DeltaBot, chat: Chat, contact: Contact) -> None:
     channel = db.get_channel_by_gid(chat.id)
     if channel:
-        me = dbot.self_contact
+        me = bot.self_contact
         if me == contact or len(chat.get_contacts()) <= 1:
             db.remove_channel(channel)
             irc_bridge.leave_channel(channel)
@@ -73,8 +55,7 @@ def deltabot_member_removed(chat: Chat, contact: Contact) -> None:
             irc_bridge.preactor.leave_channel(contact.addr, channel)
 
 
-# ======== Filters ===============
-
+@simplebot.filter(name=__name__)
 def filter_messages(message: Message, replies: Replies) -> None:
     """Process messages sent to an IRC channel.
     """
@@ -97,34 +78,35 @@ def filter_messages(message: Message, replies: Replies) -> None:
         addr, chan, ' '.join(text.split('\n')))
 
 
-# ======== Commands ===============
-
-def cmd_me(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def me(payload: str, message: Message, replies: Replies) -> None:
     """Send a message to IRC using the /me IRC command.
     """
-    chan = db.get_channel_by_gid(command.message.chat.id)
+    chan = db.get_channel_by_gid(message.chat.id)
     if not chan:
         return
 
-    addr = command.message.get_sender_contact().addr
-    text = ' '.join(command.payload.split('\n'))
+    addr = message.get_sender_contact().addr
+    text = ' '.join(payload.split('\n'))
     irc_bridge.preactor.send_action(addr, chan, text)
 
 
-def cmd_topic(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def topic(message: Message, replies: Replies) -> None:
     """Show IRC channel topic.
     """
-    chan = db.get_channel_by_gid(command.message.chat.id)
+    chan = db.get_channel_by_gid(message.chat.id)
     if not chan:
         replies.add(text='This is not an IRC channel')
     else:
         replies.add(text='Topic:\n{}'.format(irc_bridge.get_topic(chan)))
 
 
-def cmd_members(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def names(message: Message, replies: Replies) -> None:
     """Show list of IRC channel members.
     """
-    chan = db.get_channel_by_gid(command.message.chat.id)
+    chan = db.get_channel_by_gid(message.chat.id)
     if not chan:
         replies.add(text='This is not an IRC channel')
         return
@@ -136,12 +118,13 @@ def cmd_members(command: IncomingCommand, replies: Replies) -> None:
     replies.add(text=members)
 
 
-def cmd_nick(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def nick(args: list, message: Message, replies: Replies) -> None:
     """Set your IRC nick or display your current nick if no new nick is given.
     """
-    addr = command.message.get_sender_contact().addr
-    if command.payload:
-        new_nick = '_'.join(command.args)
+    addr = message.get_sender_contact().addr
+    if args:
+        new_nick = '_'.join(args)
         if not nick_re.match(new_nick):
             replies.add(
                 text='** Invalid nick, only letters and numbers are'
@@ -156,104 +139,102 @@ def cmd_nick(command: IncomingCommand, replies: Replies) -> None:
         replies.add(text='** Nick: {}'.format(db.get_nick(addr)))
 
 
-def cmd_join(command: IncomingCommand, replies: Replies) -> None:
-    """Join the given IRC channel.
-    """
-    sender = command.message.get_sender_contact()
-    if not command.payload:
+@simplebot.command
+def join(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
+    """Join the given IRC channel."""
+    sender = message.get_sender_contact()
+    if not payload:
         replies.add(text="Wrong syntax")
         return
-    if not dbot.is_admin(sender.addr) and \
-       not db.is_whitelisted(command.payload):
+    if not bot.is_admin(sender.addr) and \
+       not db.is_whitelisted(payload):
         replies.add(text="That channel isn't in the whitelist")
         return
 
-    g = dbot.get_chat(db.get_chat(command.payload))
+    g = bot.get_chat(db.get_chat(payload))
     if g and sender in g.get_contacts():
         replies.add(
             text='You are already a member of this group', chat=g)
         return
     if g is None:
-        chat = dbot.create_group(command.payload, [sender])
-        db.add_channel(command.payload, chat.id)
-        irc_bridge.join_channel(command.payload)
-        irc_bridge.preactor.join_channel(sender.addr, command.payload)
+        chat = bot.create_group(payload, [sender])
+        db.add_channel(payload, chat.id)
+        irc_bridge.join_channel(payload)
+        irc_bridge.preactor.join_channel(sender.addr, payload)
     else:
-        add_contact(g, sender)
-        chat = dbot.get_chat(sender)
+        _add_contact(g, sender)
+        chat = bot.get_chat(sender)
 
     nick = db.get_nick(sender.addr)
-    text = '** You joined {} as {}'.format(command.payload, nick)
+    text = '** You joined {} as {}'.format(payload, nick)
     replies.add(text=text, chat=chat)
 
 
-def cmd_remove(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def remove(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
     """Remove the member with the given nick from the IRC channel, if no nick is given remove yourself.
     """
-    sender = command.message.get_sender_contact()
+    sender = message.get_sender_contact()
 
-    text = command.payload
-    channel = db.get_channel_by_gid(command.message.chat.id)
+    channel = db.get_channel_by_gid(message.chat.id)
     if not channel:
-        args = command.payload.split(maxsplit=1)
+        args = payload.split(maxsplit=1)
         channel = args[0]
-        text = args[1] if len(args) == 2 else ''
-        g = dbot.get_chat(db.get_chat(channel))
+        payload = args[1] if len(args) == 2 else ''
+        g = bot.get_chat(db.get_chat(channel))
         if not g or sender not in g.get_contacts():
             replies.add(text='You are not a member of that channel')
             return
 
-    if not text:
-        text = sender.addr
-    if '@' not in text:
-        t = db.get_addr(text)
+    if not payload:
+        payload = sender.addr
+    if '@' not in payload:
+        t = db.get_addr(payload)
         if not t:
-            replies.add(text='Unknow user: {}'.format(text))
+            replies.add(text='Unknow user: {}'.format(payload))
             return
-        text = t
+        payload = t
 
-    g = dbot.get_chat(db.get_chat(channel))
+    g = bot.get_chat(db.get_chat(channel))
     for c in g.get_contacts():
-        if c.addr == text:
+        if c.addr == payload:
             g.remove_contact(c)
             if c == sender:
                 return
             s_nick = db.get_nick(sender.addr)
             nick = db.get_nick(c.addr)
             text = '** {} removed by {}'.format(nick, s_nick)
-            dbot.get_chat(db.get_chat(channel)).send_text(text)
+            bot.get_chat(db.get_chat(channel)).send_text(text)
             text = 'Removed from {} by {}'.format(channel, s_nick)
-            replies.add(text=text, chat=dbot.get_chat(c))
+            replies.add(text=text, chat=bot.get_chat(c))
             return
 
 
-# ======== Utilities ===============
-
-def run_irc() -> None:
+def _run_irc(bot: DeltaBot) -> None:
     while True:
         try:
             irc_bridge.start()
         except Exception as ex:
-            dbot.logger.exception('Error on IRC bridge: %s', ex)
+            bot.logger.exception('Error on IRC bridge: %s', ex)
             sleep(5)
 
 
-def getdefault(key: str, value: str = None) -> str:
-    val = dbot.get(key, scope=__name__)
+def _getdefault(bot: DeltaBot, key: str, value: str = None) -> str:
+    val = bot.get(key, scope=__name__)
     if val is None and value is not None:
-        dbot.set(key, value, scope=__name__)
+        bot.set(key, value, scope=__name__)
         val = value
     return val
 
 
-def get_db(bot) -> DBManager:
+def _get_db(bot) -> DBManager:
     path = os.path.join(os.path.dirname(bot.account.db_path), __name__)
     if not os.path.exists(path):
         os.makedirs(path)
     return DBManager(os.path.join(path, 'sqlite.db'))
 
 
-def add_contact(chat: Chat, contact: Contact) -> None:
+def _add_contact(chat: Chat, contact: Contact) -> None:
     img_path = chat.get_profile_image()
     if img_path and not os.path.exists(img_path):
         chat.remove_profile_image()
