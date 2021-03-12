fe0ae42348e064ee1b2621caece003cad16bf963
adbenitez <asieldbenitez@gmail.com>
2021-03-12 04:55:53 -0500

    echo: use new simplebot API

--- plugins/simplebot_echo/simplebot_echo/__init__.py
+++ plugins/simplebot_echo/simplebot_echo/__init__.py
@@ -1,21 +1,15 @@
 
-from simplebot import DeltaBot
+import simplebot
 from simplebot.bot import Replies
-from simplebot.commands import IncomingCommand
-from simplebot.hookspec import deltabot_hookimpl
 
 __version__ = '1.0.0'
 
 
-@deltabot_hookimpl
-def deltabot_init(bot: DeltaBot) -> None:
-    bot.commands.register(name="/echo", func=cmd_echo)
-
-
-def cmd_echo(command: IncomingCommand, replies: Replies) -> None:
+@simplebot.command
+def echo(payload: str, replies: Replies) -> None:
     """Echoes back received text.
 
     To use it you can simply send a message starting with
     the command '/echo'. Example: `/echo hello world`
     """
-    replies.add(text=command.payload or 'echo')
+    replies.add(text=payload or 'echo')
